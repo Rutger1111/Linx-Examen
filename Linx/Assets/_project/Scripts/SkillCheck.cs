91027e9a9eda3664d73d964b2875febf567f5a96
BlubBlub <32533@ma-web.nl>
2025-03-26 14:04:14 +0100

    time for documentation and removed some code

--- Linx/Assets/_project/Scripts/SkillCheck.cs
+++ Linx/Assets/_project/Scripts/SkillCheck.cs
@@ -5,26 +5,20 @@ using UnityEngine.Events;
 public class SkillCheck : MonoBehaviour
 {
     public List<UnityEvent> _unityEvents = new List<UnityEvent>();
-
-    public bool battle;
+    public int randomNumber;
+    
     void Start()
     {
-        
+        MiniGame();
     }
     
-    void Update()
-    {
-        if (battle)
-        {
-            battle = false;
-            int randomNumber = Random.Range(0, _unityEvents.Count);
+    
 
-            for (int i = 0; i < _unityEvents.Count; i++)
-            {
-                _unityEvents[randomNumber].Invoke();   
-            }
-        }
+    void MiniGame()
+    {
+        randomNumber = Random.Range(0, _unityEvents.Count);
+
             
-        
+        _unityEvents[randomNumber].Invoke();   
     }
 }
