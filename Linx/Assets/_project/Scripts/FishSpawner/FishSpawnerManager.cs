a953279544d4c4cd94eb6713bae5e648661d2632
BlubBlub <32533@ma-web.nl>
2025-03-26 13:40:19 +0100

    added the correct namespace

--- Linx/Assets/_project/Scripts/FishSpawner/FishSpawnerManager.cs
+++ Linx/Assets/_project/Scripts/FishSpawner/FishSpawnerManager.cs
@@ -4,101 +4,106 @@ using UnityEngine;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
-public class FishSpawnerManager : MonoBehaviour
+namespace FishSpawner
 {
-    [SerializeField] 
-    private List<SpawnContainer> _spawnInformation = new List<SpawnContainer>();
-    
-    [SerializeField] 
-    private GameObject _playerRefence;
-    
-    void Update()
+    public class FishSpawnerManager : MonoBehaviour
     {
-       SpawnHandle();
-    }
-    
-    void SpawnHandle()
-    {
-        foreach (SpawnContainer con in _spawnInformation)
+        [SerializeField] private List<SpawnContainer> spawnInformation = new List<SpawnContainer>();
+
+        [SerializeField] private GameObject _playerRefence;
+
+        void Update()
         {
-            con.enemiesSpawned.RemoveAll(enemy => enemy == null);
-            if (Vector3.Distance(con.spawnPoint.transform.position, _playerRefence.transform.position) > 10f && con.hasStartedSpawn == true)
+            SpawnHandle();
+        }
+
+        void SpawnHandle()
+        {
+            foreach (SpawnContainer con in spawnInformation)
+            {
+                con.enemiesSpawned.RemoveAll(enemy => enemy == null);
+                if (Vector3.Distance(con.spawnPoint.transform.position, _playerRefence.transform.position) > 10f &&
+                    con.hasStartedSpawn == true)
+                {
+                    if (con.enemiesSpawned.Count + 1 <= con.maxEnemies)
+                    {
+                        con.spawnCoolDown -= Time.deltaTime;
+                        if (con.spawnCoolDown <= 0)
+                        {
+                            con.spawnCoolDown = con.spawnCoolDownReset;
+
+                            InitializeSpawning();
+                        }
+                    }
+                }
+
+                if (con.hasStartedSpawn == false)
+                {
+                    StartSpawnHandle();
+                }
+            }
+        }
+
+        void StartSpawnHandle()
+        {
+            foreach (SpawnContainer con in spawnInformation)
+            {
+                con.enemiesSpawned.RemoveAll(enemy => enemy == null);
+
+                if (con.enemiesSpawned.Count + 1 <= con.maxEnemies)
+                {
+                    InitializeSpawning();
+                }
+                else
+                {
+                    con.hasStartedSpawn = true;
+                }
+            }
+        }
+
+        void InitializeSpawning()
+        {
+            foreach (SpawnContainer con in spawnInformation)
             {
                 if (con.enemiesSpawned.Count + 1 <= con.maxEnemies)
                 {
-                    con.spawnCoolDown -= Time.deltaTime;
-                    if (con.spawnCoolDown <= 0)
-                    {
-                        con.spawnCoolDown = con.spawnCoolDownReset;
+                    float randomX = Random.Range(-con.spawnArea.x / 2f, con.spawnArea.x / 2f);
+                    float randomY = Random.Range(-con.spawnArea.y / 2f, con.spawnArea.y / 2f);
 
-                        InitializeSpawning();
-                    }
+                    Vector3 randomPosition = con.spawnPoint.transform.position + new Vector3(randomX, randomY);
+
+                    GameObject enemy = Instantiate(con.enemyPrefab, randomPosition, Quaternion.identity);
+
+                    con.enemiesSpawned.Add(enemy);
+                    enemy.transform.parent = con.spawnParent.transform;
                 }
             }
-
-            if (con.hasStartedSpawn == false)
-            {
-                StartSpawnHandle();
-            }
         }
-    }
-    
-    void StartSpawnHandle()
-    {
-        foreach (SpawnContainer con in _spawnInformation)
+
+        private void OnDrawGizmos()
         {
-            con.enemiesSpawned.RemoveAll(enemy => enemy == null);
-           
-            if (con.enemiesSpawned.Count + 1 <= con.maxEnemies)
+            foreach (SpawnContainer con in spawnInformation)
             {
-                InitializeSpawning();
-            }
-            else 
-            {
-                con.hasStartedSpawn = true;
+                Gizmos.DrawWireCube(con.spawnPoint.transform.position, con.spawnArea);
             }
         }
     }
 
-    void InitializeSpawning()
+    [Serializable]
+    public class SpawnContainer
     {
-        foreach (SpawnContainer con in _spawnInformation)
-        {
-            if (con.enemiesSpawned.Count + 1 <= con.maxEnemies)
-            {
-                float randomX = Random.Range(-con.spawnArea.x / 2f, con.spawnArea.x / 2f);
-                float randomY = Random.Range(-con.spawnArea.y / 2f, con.spawnArea.y / 2f);
+        public GameObject spawnPoint;
+        public GameObject enemyPrefab;
+        public Vector2 spawnArea;
 
-                Vector3 randomPosition = con.spawnPoint.transform.position + new Vector3(randomX, randomY);
+        public float spawnCoolDown;
+        public float spawnCoolDownReset;
 
-                GameObject enemy = Instantiate(con.enemyPrefab, randomPosition, Quaternion.identity);
+        public List<GameObject> enemiesSpawned;
+        public int maxEnemies;
 
-                con.enemiesSpawned.Add(enemy);
-                enemy.transform.parent = con.spawnParent.transform;     
-            }
-        }
+        public GameObject spawnParent;
+
+        public bool hasStartedSpawn;
     }
-    private void OnDrawGizmos()
-    {
-        foreach (SpawnContainer con in _spawnInformation)
-        {
-            Gizmos.DrawWireCube(con.spawnPoint.transform.position, con.spawnArea);    
-        }
-    }
-}
-[Serializable] public class SpawnContainer
-{
-    public GameObject spawnPoint;
-    public GameObject enemyPrefab;
-    public Vector2 spawnArea;
-    
-    public float spawnCoolDown;
-    public float spawnCoolDownReset;
-
-    public List<GameObject> enemiesSpawned;
-    public int maxEnemies;
-
-    public GameObject spawnParent;
-
-    public bool hasStartedSpawn;
 }
