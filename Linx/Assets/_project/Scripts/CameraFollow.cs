0897c88e18a3245177928405ebc4de99171710f9
BlubBlub <32533@ma-web.nl>
2025-03-31 13:53:57 +0200

    push for update

--- Linx/Assets/_project/Scripts/CameraFollow.cs
+++ Linx/Assets/_project/Scripts/CameraFollow.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -7,22 +9,59 @@ public class CameraFollow : MonoBehaviour
     public float speed;
 
     public float distanceForMovement;
-    
+
+    public Vector3 newPosition;
+    public float notmoving;
+
+    public Vector3 movetoPlayerPos;
+
+    public bool isnotmoving;
+
+
+    private void Start()
+    {
+        newPosition.z = -10f;
+        movetoPlayerPos.z = -10f;
+    }
+
     void Update()
     {
         Vector3 direction = player.transform.position - transform.position;
 
         if (Mathf.Abs(direction.x) > distanceForMovement || Mathf.Abs(direction.y) > distanceForMovement)
         {
-            Vector3 newPosition = transform.position + new Vector3(direction.x, direction.y, 0).normalized * speed * Time.deltaTime;
+            newPosition = transform.position + new Vector3(direction.x, direction.y, 0) * speed * Time.deltaTime;
         
-        
+            
             newPosition.z = transform.position.z;
 
-            transform.position = newPosition;    
-            
+            isnotmoving = false;
+
         }
 
-        
+        if ( transform.position != newPosition && isnotmoving == false && notmoving >= 0)
+        {
+            
+            transform.position = newPosition;
+            
+            if (notmoving <= 5f)
+            {
+                notmoving++;
+            }
+
+        }
+        else if(transform.position == newPosition && notmoving >= 0)
+        {
+            notmoving -= Time.deltaTime;
+
+            if (notmoving <= 0)
+            { 
+                notmoving++;
+               
+                movetoPlayerPos = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z), speed * Time.deltaTime);
+
+                transform.position = movetoPlayerPos;
+            }
+        }
     }
 }
