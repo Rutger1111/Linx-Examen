c2e7c452727bcb4ef8d10e3db42df1ded7965c56
Rutger1111 <djrutgervandelaar@gmail.com>
2025-04-01 11:15:44 +0200

    fixed

--- Linx/Assets/_project/Scripts/PlayerDeath/Death.cs
+++ Linx/Assets/_project/Scripts/PlayerDeath/Death.cs
@@ -6,16 +6,4 @@ public class Death : ICommand
     public override void Invoke(Fish fish)
     {
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
