7e37d63f76905246873a1e3c347d7fc4b6222260
Rutger1111 <djrutgervandelaar@gmail.com>
2025-05-13 10:59:35 +0200

    snap smol fix

--- Linx/Assets/_New Game/Scripts/Snapping/Snap.cs
+++ Linx/Assets/_New Game/Scripts/Snapping/Snap.cs
@@ -1,5 +1,6 @@
 using System;
 using FishSystem;
+using ParrelSync.NonCore;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -13,9 +14,15 @@ public class Snap : ICommand
     public int isPickedUp;
 
     public GameObject UIplace;
-    private Vector3 pos;
-    private quaternion rot;
+    private Vector3 _pos;
+    private quaternion _rot;
+    private Vector3 _hook1Pos;
+    private quaternion _hook1Rot;
+    private Vector3 _hook2Pos;
+    private quaternion _hook2Rot;
     public SnapPosition _snapPosition;
+    public GameObject _hookObject1;
+    public GameObject _hookObject2;
     void Start()
     {
         GetComponent<Rigidbody>().isKinematic = false;
@@ -23,8 +30,12 @@ public class Snap : ICommand
     void OnTriggerStay(Collider other)
     {
         if(placed >= 1){
-            transform.position = pos;
-            transform.rotation = rot;
+            transform.position = _pos;
+            transform.rotation = _rot;
+            _hookObject1.transform.position = _hook1Pos;
+            _hookObject1.transform.rotation = _hook1Rot;
+            _hookObject1.transform.position = _hook2Pos;
+            _hookObject1.transform.rotation = _hook2Rot;
         }
         if (other.gameObject.tag == "BuildPosition")
         {
@@ -72,33 +83,15 @@ public class Snap : ICommand
     {
         if(GetComponent<Snap>().isPickedUp > 0){
             
-            GameObject referenceObject = col.gameObject.transform.parent.gameObject;
-            
-            // Get the forward direction in the horizontal plane
-            Vector3 refForward = referenceObject.transform.forward;
-            refForward.y = 0;
-            refForward.Normalize();
-
-            // Get 90° perpendicular direction (right turn)
-            Vector3 perpDirection = new Vector3(-refForward.z, 0, refForward.x);
-
-            if (perpDirection != Vector3.zero)
-            {
-                Quaternion targetRotation = Quaternion.LookRotation(perpDirection, Vector3.up);
-                transform.rotation = targetRotation;
-                transform.rotation = new quaternion(transform.rotation.x,transform.rotation.y + 90,transform.rotation.z + 90,0);
-                if (col.tag != "Ground" ){
-                    print("came here 1");
-                    transform.position = new Vector3(col.transform.position.x, transform.position.y, col.transform.position.z);
-                }
-                else if (col.tag == "Ground"){
-                    print("came here 2");
-                    col.tag = "Untagged";
-                    col.enabled = false;
-                }
-                pos = transform.position;
-                rot = transform.rotation;
-            }            
+            print("werktteeee");
+            transform.rotation = new quaternion(col.transform.rotation.x,col.transform.rotation.y,col.transform.rotation.z,0);
+            _pos = col.transform.position;
+            _rot = new quaternion(col.gameObject.transform.rotation.x ,col.gameObject.transform.rotation.y,col.gameObject.transform.rotation.z,col.transform.rotation.w);
+            _hook1Pos = _hookObject1.transform.position;
+            _hook1Rot = _hookObject1.transform.rotation;
+            _hook2Pos = _hookObject1.transform.position;
+            _hook2Rot = _hookObject1.transform.rotation;
+                    
         }
     }
 
